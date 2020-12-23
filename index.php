52c5456a5bafee54c4552d24a7a0ecb662e0a941
N. K <n.kudaba@gmail.com>
2020-12-23 22:07:52 +0200

    try arrays

--- index.php
+++ index.php
@@ -8,8 +8,15 @@
 		// a comment
 		$name = "PHP Store";
 		$credit = 1000;
+		$products['computer'] = 750;
+		$products['car'] = 15000;
+		$products['phone'] = 500;
+		$products['toaster'] = 25;
+
 		echo "<h1>Welcome to ".$name.".</h1>";
-		echo "<h2>You have ".$credit." in your wallet.</h2>";
+		echo "<h2>You have $".$credit." in your wallet.</h2>";
+		echo "<p>A car costs $".$products['car']."</p>";
+		
 		?>
 	</body>
 </html>
